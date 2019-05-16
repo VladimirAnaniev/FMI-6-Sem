ca787ef6873e054f6eeb361be4ec27db39296627
Vladimir Ananiev <vladeto97@gmail.com>
2019-05-17 00:08:54 +0300

    scala second homework - Validated

--- Scala/src/main/scala/homework2/Validated.scala
+++ Scala/src/main/scala/homework2/Validated.scala
@@ -1,19 +1,41 @@
 package homework2
 
 sealed trait Validated[+E, +A] {
-  def isValid: Boolean = ???
+  def isValid: Boolean = this match {
+    case Invalid(_) => false
+    case Valid(_) => true
+  }
 
-  def getOrElse[B >: A](default: => B): B = ???
+  def get[B >: A] = this match {
+    case Valid(a) => a
+    case _ => throw new IllegalAccessError("Called get on invalid Validated!")
+  }
 
-  def orElse[F >: E, B >: A](default: => Validated[F, B]): Validated[F, B] = ???
+  def getOrElse[B >: A](default: => B): B = this match {
+    case Invalid(_) => default
+    case Valid(value) => value;
+  }
 
-  def zip[EE >: E, B](vb: Validated[EE, B]): Validated[EE, (A, B)] = ???
+  def orElse[F >: E, B >: A](default: => Validated[F, B]): Validated[F, B] = if (isValid) this else default
 
-  def map[B](f: A => B): Validated[E, B] = ???
+  def zip[EE >: E, B](vb: Validated[EE, B]): Validated[EE, (A, B)] = (this, vb) match {
+    case (Valid(a), Valid(b)) => Valid((a, b))
+    case (Invalid(a), Valid(_)) => Invalid(a)
+    case (Valid(_), Invalid(b)) => Invalid(b)
+    case (Invalid(a), Invalid(b)) => Invalid(a ++ b)
+  }
 
-  def map2[EE >: E, B, R](vb: Validated[EE, B])(f: (A, B) => R): Validated[EE, R] = ???
+  def map[B](f: A => B): Validated[E, B] = this match {
+    case Valid(a) => Valid(f(a))
+    case Invalid(b) => Invalid(b)
+  }
 
-  def flatMap[EE >: E, B](f: A => Validated[EE, B]): Validated[EE, B] = ???
+  def map2[EE >: E, B, R](vb: Validated[EE, B])(f: (A, B) => R): Validated[EE, R] = zip(vb) map f.tupled
+
+  def flatMap[EE >: E, B](f: A => Validated[EE, B]): Validated[EE, B] = map(f) match {
+    case Valid(a) => a
+    case Invalid(b) => Invalid(b)
+  }
 
   def fold[B](invalid: Chain[E] => B, valid: A => B): B = this match {
     case Invalid(errors) => invalid(errors)
@@ -24,6 +46,7 @@ sealed trait Validated[+E, +A] {
 }
 
 case class Valid[+A](a: A) extends Validated[Nothing, A]
+
 case class Invalid[+E](errors: Chain[E]) extends Validated[E, Nothing]
 
 object Invalid {
@@ -34,26 +57,60 @@ object Validated {
   def sequence[E, A](xs: List[Validated[E, A]]): Validated[E, List[A]] = ???
 
   implicit class ValidatedTuple2[EE, A, B](val tuple: (Validated[EE, A], Validated[EE, B])) extends AnyVal {
-    def zip: Validated[EE, (A, B)] = ???
-    def zipMap[R](f: (A, B) => R): Validated[EE, R] = ???
+    def zip: Validated[EE, (A, B)] = tuple._1 zip tuple._2
+
+    def zipMap[R](f: (A, B) => R): Validated[EE, R] = zip map f.tupled
   }
 
   implicit class ValidatedTuple3[EE, A, B, C](val tuple: (Validated[EE, A], Validated[EE, B], Validated[EE, C])) extends AnyVal {
-    def zip: Validated[EE, (A, B, C)] = ???
-    def zipMap[R](f: (A, B, C) => R): Validated[EE, R] = ???
+    def zip: Validated[EE, (A, B, C)] = {
+      val values = tuple.productIterator.toList.map(_.asInstanceOf[Validated[EE, Any]])
+
+      if (values.forall(_.isValid))
+        Valid((tuple._1.get, tuple._2.get, tuple._3.get))
+      else
+        // TODO: Hacky solution.. try to clean it up
+        values.reduce((a, b) => a.zip(b).asInstanceOf[Validated[EE, (A, B, C)]]).asInstanceOf[Validated[EE, (A, B, C)]]
+    }
+
+    def zipMap[R](f: (A, B, C) => R): Validated[EE, R] = zip map f.tupled
   }
 
   implicit class ValidatedTuple4[EE, A, B, C, D]
   (val tuple: (Validated[EE, A], Validated[EE, B], Validated[EE, C], Validated[EE, D])) extends AnyVal {
-    def zip: Validated[EE, (A, B, C, D)] = ???
-    def zipMap[R](f: (A, B, C, D) => R): Validated[EE, R] = ???
+    def zip: Validated[EE, (A, B, C, D)] = {
+      val values = tuple.productIterator.toList.map(_.asInstanceOf[Validated[EE, Any]])
+
+      if (values.forall(_.isValid))
+        Valid((tuple._1.get, tuple._2.get, tuple._3.get, tuple._4.get))
+      else
+      // TODO: Hacky solution.. try to clean it up
+        values.reduce((a, b) => a.zip(b).asInstanceOf[Validated[EE, (A, B, C, D)]]).asInstanceOf[Validated[EE, (A, B, C, D)]]
+    }
+
+    def zipMap[R](f: (A, B, C, D) => R): Validated[EE, R] = zip map f.tupled
   }
 
   implicit class ValidatedTuple5[EE, A, B, C, D, E]
   (val tuple: (Validated[EE, A], Validated[EE, B], Validated[EE, C], Validated[EE, D], Validated[EE, E])) extends AnyVal {
-    def zip: Validated[EE, (A, B, C, D, E)] = ???
-    def zipMap[R](f: (A, B, C, D, E) => R): Validated[EE, R] = ???
+    def zip: Validated[EE, (A, B, C, D, E)] = {
+      val values = tuple.productIterator.toList.map(_.asInstanceOf[Validated[EE, Any]])
+
+      if (values.forall(_.isValid))
+        Valid((tuple._1.get, tuple._2.get, tuple._3.get, tuple._4.get, tuple._5.get))
+      else
+      // TODO: Hacky solution.. try to clean it up
+        values.reduce((a, b) => a.zip(b).asInstanceOf[Validated[EE, (A, B, C, D, E)]]).asInstanceOf[Validated[EE, (A, B, C, D, E)]]
+    }
+
+    def zipMap[R](f: (A, B, C, D, E) => R): Validated[EE, R] = zip map f.tupled
   }
 
-  // ??? TODO: Add toValidated to Option instances
+  implicit class ValidatedOption[A](val option: Option[A]) extends AnyVal {
+    def toValidated[E](onEmpty: E): Validated[E, A] = option match {
+      case None => Invalid(onEmpty)
+      case Some(value) => Valid(value)
+    }
+  }
+
 }
--- Scala/src/test/scala/homework2/ValidatedTest.scala
+++ Scala/src/test/scala/homework2/ValidatedTest.scala
@@ -2,6 +2,8 @@ package homework2
 
 import org.scalatest.{FlatSpec, FunSuite, Matchers}
 
+import homework2.Validated.ValidatedTuple3
+
 class ValidatedTest extends FlatSpec with Matchers {
   "zip" should "combine valid instances" in {
     Valid(1).zip(Valid("a")) shouldEqual Valid((1, "a"))
@@ -9,5 +11,33 @@ class ValidatedTest extends FlatSpec with Matchers {
 
   it should "combine errors from invalid instances" in {
     Invalid(1).zip(Invalid(Chain(2, 3))) shouldEqual Invalid(Chain(1, 2, 3))
+  }
+
+  "map" should "transform valid values" in {
+    Valid(2).map(_ * 2) shouldEqual Valid(4)
+  }
+
+  it should "preserve errors" in {
+    Invalid(5).map(_ => 3).map(_ => 7) shouldEqual Invalid(5)
+  }
+
+  "flatMap" should "transform valid values" in {
+    Valid(2).flatMap(x => Valid(x * 2)) shouldEqual Valid(4)
+  }
+
+  it should "preserve errors?" in {
+    Invalid(2).flatMap(_ => Invalid(5)) shouldEqual Invalid(Chain(2))
+  }
+
+  "ValidatedTuple3" should "zip valid values" in {
+    val zipped = (Valid(1), Valid(2), Valid(3)).zip
+
+    zipped shouldEqual Valid((1, 2, 3))
+  }
+
+  it should "chain invalid values" in {
+    val zipped = (Invalid(2), Valid(5), Invalid(Chain(5,6))).zip
+
+    zipped shouldEqual Invalid(Chain(2, 5, 6))
   }
 }
